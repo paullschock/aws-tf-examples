05fad83075d77c481402ce6f102d1fca3c508dd8
Paul Schock <paul.schock@resmed.com>
2019-01-14 14:23:38 -0800

    Remove public IP address from instance

--- example.tf
+++ example.tf
@@ -9,6 +9,8 @@ resource "aws_instance" "web" {
   availability_zone = ""
   instance_type = "t2.micro"
 
+  associate_public_ip_address = false
+
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
