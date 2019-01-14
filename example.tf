4d4f4d09f7a3b72991375e53bdffe2dc0e34ec67
Paul Schock <paul.schock@resmed.com>
2019-01-14 10:40:52 -0800

    Initial test of ELB and alias record

--- example.tf
+++ example.tf
@@ -3,23 +3,87 @@ provider "aws" {
   secret_key = "${var.secret_key}"
   region     = "${var.region}"
 }
-resource "aws_s3_bucket" "example" {
-  bucket = "terraform-getting-started-guide-1234"
-  acl = "private"
-}
-resource "aws_instance" "example" {
-  ami           = "ami-2757f631"
+
+resource "aws_instance" "web" {
+  ami           = "ami-2d39803a"
+  availability_zone = ""
   instance_type = "t2.micro"
 
-  provisioner "local-exec" {
-  command = "echo ${aws_instance.example.public_ip} > ip_address.txt"
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p "${var.server_port}" &
+              EOF
+  tags {
+    Name = "test web server"
+  }
+  vpc_security_group_ids = ["${aws_security_group.instance.id}"]
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+  ingress {
+    from_port = "${var.server_port}"
+    to_port = "${var.server_port}"
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
-resource "aws_eip" "ip" {
-  instance = "${aws_instance.example.id}"
-  depends_on = ["aws_s3_bucket.example"]
+
+resource "aws_security_group" "elb" {
+  name = "terraform-example-elb"
+  ingress {
+    from_port = "${var.server_port}"
+    to_port = "${var.server_port}"
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
-resource "aws_instance" "another" {
-  ami = "ami-b374d5a5"
-  instance_type = "t2.micro"
+
+resource "aws_elb" "example" {
+  name = "terraform-asg-example"
+  security_groups = ["${aws_security_group.elb.id}"]
+  availability_zones = ["us-east-1a"]
+  health_check {
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+    timeout = 3
+    interval = 30
+    target = "HTTP:${var.server_port}/"
+  }
+  listener {
+    lb_port = "${var.server_port}"
+    lb_protocol = "http"
+    instance_port = "${var.server_port}"
+    instance_protocol = "http"
+  }
+}
+
+resource "aws_elb_attachment" "example" {
+  elb      = "${aws_elb.example.id}"
+  instance = "${aws_instance.web.id}"
+}
+
+module aws_route53_zone "testZone" {
+  name = "aws.paullschock.com"
+
+  provisioner "local-exec" {
+    command = "echo ${aws_route53_zone.testZone.name_servers} > name_servers.txt"
+  }
+}
+
+module aws_route53_record "www" {
+  zone_id = "${aws_route53_zone.testZone.zone_id}"
+  name = "aws.paullschock.com"
+  type = "A"
+
+  alias {
+    name = "${aws_elb.example.dns_name}"
+    zone_id = "${aws_elb.example.zone_id}"
+    evaluate_target_health = true
+  }
+}
+
+output "public_ip" {
+  value = "${aws_instance.example.public_ip}"
 }
